521256e6868e96ebdff04317fce65e9eabb73447
Muzaffar Murodovich <muzaffarmurodogli@gmail.com>
2025-11-17 20:02:01 +0500

    modelform

--- users/views.py
+++ users/views.py
@@ -1,27 +1,27 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views import View
+from users.forms import UserCreateForm
 
 class RegisterView(View):
     def get(self, request):
-        return render(request,'users/register.html')
+        create_form = UserCreateForm()
+        context = {
+            'form': create_form,
+        }
+        return render(request,'users/register.html', context)
 
     def post(self, request):
-        username = request.POST['username']
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        password = request.POST['password']
-        password2 = request.POST['password2']
-        email = request.POST['email']
-        if password == password2:
-            user = User.objects.create_user(
-                username=username,
-                first_name=first_name,
-                last_name=last_name,
-                email=email)
-            user.set_password(password)
-            user.save()
-        return redirect('login')
+        create_form = UserCreateForm(data=request.POST)
+
+        if create_form.is_valid():
+            create_form.save()
+            return redirect('login')
+        else:
+            context = {
+                'form': create_form,
+            }
+            return render(request,'users/register.html', context)
 
 class LoginView(View):
     def get(self, request):
