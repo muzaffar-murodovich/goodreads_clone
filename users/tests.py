a480524ab91a65d8e7150c1fee6e6ab3cc521263
Muzaffar Murodovich <muzaffarmurodogli@gmail.com>
2025-11-18 20:58:36 +0500

    +LoginView expanding, test, login form

--- users/tests.py
+++ users/tests.py
@@ -1,4 +1,4 @@
-from urllib import response
+from django.contrib.auth import get_user
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
@@ -6,66 +6,117 @@ from django.urls import reverse
 class RegistrationTestCase(TestCase):
     def test_user_account_is_created(self):
         self.client.post(
-            reverse('register'),
+            reverse("register"),
             data={
-                'username': 'muzaffar',
-                'first_name': 'muzaffar',
-                'last_name': 'joraboyev',
-                'email': 'invalid',
-                'password': 'anypassword',
+                "username": "muzaffar",
+                "first_name": "muzaffar",
+                "last_name": "joraboyev",
+                "email": "muzaffarmurodogli@gmail.com",
+                "password": "anypassword"
             }
         )
 
-        user = User.objects.get(username='')
+        user = User.objects.get(username="muzaffar")
 
-        self.assertEqual(user.username, '')
-        self.assertEqual(user.first_name, '')
-        self.assertEqual(user.last_name, '')
-        self.assertEqual(user.email, '')
-        self.assertNotEqual(user.password, '')
-        self.assertTrue(user.check_password('somepassword'))
+        self.assertEqual(user.first_name, "muzaffar")
+        self.assertEqual(user.last_name, "joraboyev")
+        self.assertEqual(user.email, "muzaffarmurodogli@gmail.com")
+        self.assertNotEqual(user.password, "anypassword")
+        self.assertTrue(user.check_password("anypassword"))
 
     def test_required_fields(self):
         response = self.client.post(
-            reverse('register'),
+            reverse("register"),
             data={
-                'first_name':'muzaffar',
-                "email":"muzaffarmurodogli@gmail.com",
+                "first_name": "muzaffar",
+                "email": "muzaffarmurodogli@gmail.com"
             }
         )
+
         user_count = User.objects.count()
+
         self.assertEqual(user_count, 0)
-        self.assertFormError(response, 'form', 'username', 'This field is required.')
+        self.assertFormError(response, "form", "username", "This field is required.")
+        self.assertFormError(response, "form", "password", "This field is required.")
 
     def test_invalid_email(self):
         response = self.client.post(
-            reverse('register'),
+            reverse("register"),
             data={
-                'username':'muzaffar',
-                'first_name':'muzaffar',
-                'last_name':'joraboyev',
-                'email':'invalid',
-                'password':'anypassword',
+                "username": "muzaffar",
+                "first_name": "muzaffar",
+                "last_name": "joraboyev",
+                "email": "invalid-email",
+                "password": "anypassword"
             }
         )
 
         user_count = User.objects.count()
 
         self.assertEqual(user_count, 0)
-        self.assertFormError(response, 'form', 'email', 'Enter a valid email address.')
+        self.assertFormError(response, "form", "email", "Enter a valid email address.")
 
     def test_unique_username(self):
+        user = User.objects.create(username="muzaffar", first_name="muzaffar")
+        user.set_password("somepass")
+        user.save()
+
         response = self.client.post(
-            reverse('register'),
+            reverse("register"),
             data={
-                'username':'muzaffar',
-                'first_name':'muzaffar',
-                'last_name':'joraboyev',
-                'email':'muzaffarmurodogli@gmail.com',
-                'password':'anypassword',
+                "username": "muzaffar",
+                "first_name": "muzaffar",
+                "last_name": "joraboyev",
+                "email": "muzaffarmurodogli@gmail.com",
+                "password": "anypassword"
             }
         )
 
         user_count = User.objects.count()
         self.assertEqual(user_count, 1)
-        self.assertFormError(response, 'form', 'username', 'This username has already been taken.')
+        self.assertFormError(response, "form", "username", "A user with that username already exists.")
+
+
+class LoginTestCase(TestCase):
+    def test_successful_login(self):
+        db_user = User.objects.create_user(username="muzaffar", firstname="muzaffar")
+        db_user.set_password("somepassword")
+        db_user.save()
+
+        self.client.post(
+            reverse("login"),
+            data={
+                "username": "muzaffar",
+                "password": "anypassword"
+            }
+        )
+
+        user = get_user(self.client)
+        self.assertTrue(user.is_authenticated)
+
+    def test_wrong_credentials(self):
+        db_user = User.objects.create_user(username="muzaffar", firstname="muzaffar")
+        db_user.set_password("somepassword")
+        db_user.save()
+
+        self.client.post(
+            reverse("login"),
+            data={
+                "username": "wrong-username",
+                "password": "anypassword"
+            }
+        )
+
+        user = get_user(self.client)
+        self.assertFalse(user.is_authenticated)
+
+        self.client.post(
+            reverse("login"),
+            data={
+                "username": "muzaffar",
+                "password": "wrong-password"
+            }
+        )
+
+        user = get_user(self.client)
+        self.assertFalse(user.is_authenticated)
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.views import View
 from users.forms import UserCreateForm
+
 
 class RegisterView(View):
     def get(self, request):
@@ -25,4 +27,16 @@ class RegisterView(View):
 
 class LoginView(View):
     def get(self, request):
-        return render(request,'users/login.html')
+        login_form = AuthenticationForm()
+
+        return render(request,'users/login.html', context={'login_form': login_form})
+
+    def post(self, request):
+        login_form = AuthenticationForm(data=request.POST)
+        if login_form.is_valid():
+            user = login_form.get_user()
+            login(request, user)
+
+            return redirect('landing_page')
+        else:
+            return render(request,'users/login.html', context={'login_form': login_form})
