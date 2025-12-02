ff53690b3790b733e4d61e6627544600554bae5d
Muzaffar Murodovich <muzaffarmurodogli@gmail.com>
2025-12-02 16:32:15 +0500

    sqlite instead of postgresql

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -93,13 +93,9 @@ WSGI_APPLICATION = 'goodreads.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
